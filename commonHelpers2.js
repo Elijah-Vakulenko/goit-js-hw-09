88ee46cb176a5c64d7204c9f541eca3636d07a83
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-07 22:18:20 +0000

    Deploying to gh-pages from @ Elijah-Vakulenko/goit-js-hw-09@42dae26472458e006c5cbf0ac7ed0b5ee97153e7 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=document.querySelector(".feedback-form");document.querySelector("input");document.querySelector("button");e.addEventListener("submit",o);function o(t){t.preventDefault(),e.reset()}
+//# sourceMappingURL=commonHelpers2.js.map
